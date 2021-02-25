2079fc6b651065b8e19be6340fcd1f3fecccd632
Sachin Holla <sacholla@amazon.com>
2021-02-25 09:42:01 -0800

    added auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// CHANGES
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+// CHANGES - END
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
